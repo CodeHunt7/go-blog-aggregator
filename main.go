c7b43ac8784190b5aba86e3936cc521debcbbaeb
CodeHunt7 <mishavasilenko1@yandex.ru>
2026-03-15 15:13:09 +0300

    Added creation of users using database, updated login function

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/CodeHunt7/go-blog-aggregator/internal/config"
+	"github.com/CodeHunt7/go-blog-aggregator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 // Делаем структуру-рюкзак для всех данных (функции, бд) передаваеммых в команды
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 // Cтруктура для CLI-команды
@@ -20,19 +27,71 @@ type command struct {
 
 // Login handler function
 func handlerLogin(s *state, cmd command) error {
-	// Нет логина - ошибка
+	// Проверяетм, что в аргументах есть имя
 	if len(cmd.agrs) == 0 {
 		return fmt.Errorf("Username is required")
 	}
 
+	// Проверяем, что пользователь есть в БД
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, cmd.agrs[0])
+	if err != nil {
+		return fmt.Errorf("user %s does not exist", cmd.agrs[0])
+	}
+
 	// Устанавливаем наше имя пользователя
-	err := s.cfg.SetUser(cmd.agrs[0])
+	err = s.cfg.SetUser(cmd.agrs[0])
 	if err != nil {
 		fmt.Printf("Error setting user: %v\n", err)
 		return err
 	}
 
-	fmt.Printf("User %s has been set\n", s.cfg.CurrentUserName)
+	fmt.Printf("User %s has been logged in\n", s.cfg.CurrentUserName)
+	return nil
+}
+
+// Register handler fucntion
+func handlerRegister(s *state, cmd command) error {
+	// Проверяетм, что в аргументах есть имя
+		if len(cmd.agrs) == 0 {
+		return fmt.Errorf("Username is required")
+	}
+	
+	// Создаем переменные для подставления в CreateUser функцию от sclc
+	id := uuid.New()
+	now := time.Now()
+	name := cmd.agrs[0]
+	ctx := context.Background()
+
+	// Формируем структуру из них
+	params := database.CreateUserParams{
+		ID:        id,
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	}
+
+	// Проверяем, что такого пользователя нет в БД, если есть - ошибка
+	_, err := s.db.GetUser(ctx, name)
+	if err == nil {
+		return fmt.Errorf("user %s already exists", name)
+	}
+
+	// Пытаемся создать пользователя в БД
+	user, err := s.db.CreateUser(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Устанавливаем созданного пользователя в конфиг
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		fmt.Printf("Error setting user: %v\n", err)
+		return err
+	}
+
+	fmt.Printf("User %s was created\n", name)
+	fmt.Println(user)
 	return nil
 }
 
@@ -61,21 +120,32 @@ func (c *commands) register(name string, f func(*state, command) error) {
 } 
 
 func main() {
-	// Чтаем конфиг при запуске
+	// Читаем конфиг при запуске
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Printf("Error reading config: %v\n", err)
 		return
 	}
 
+	// Инициализируем подключение к БД
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		fmt.Printf("Error opening DB: %v\n", err)
+		return
+	}
+	
+	dbQueries := database.New(db)
+
 	// Инициализируем состояние и команды
 	var s state	
 	s.cfg = &cfg
+	s.db = dbQueries
 	var c commands
 	c.handlerMap = make(map[string]func(*state, command) error)
 
 	// Регистрируем команду login
 	c.register("login", handlerLogin)
+	c.register("register", handlerRegister)
 
 	// Берем аргументы из ввода пользователя
 	if len(os.Args) < 2 {
